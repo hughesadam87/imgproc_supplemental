d6730a09a802498b1aef6196fba64352ce5be731
hugadams <hugadams@gwmail.gwu.edu>
2014-03-18 15:28:10 -0400

    adding _shared

---
+++ Notebooks/_shared.py
@@ -0,0 +1,5 @@
+""" Shared utilities to reduce boilerplate between examples.  STuff like importing test images and so one.  Wanted to just be able to import a function from a  notebook, but this is fairly involved:
+http://nbviewer.ipython.org/github/ipython/ipython/blob/master/examples/notebooks/Importing%20Notebooks.ipynb
+"""
+
+
